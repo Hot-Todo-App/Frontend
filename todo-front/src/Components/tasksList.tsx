514887ef3b5dbcae4609b8fc7f0d05b627685d9a
Yuvalkop22 <Kopelman050@gmail.com>
2023-05-16 02:12:16 +0300

    delete fixed in front

--- todo-front/src/Components/tasksList.tsx
+++ todo-front/src/Components/tasksList.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react';
+import {useRouter} from 'next/router';
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import List from '@mui/material/List';
@@ -12,7 +13,10 @@ import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 
 export default function TasksList() {
+  
   const [allTasks, setAllTasks] = useState([]);
+  const router = useRouter();
+
   useEffect(() => {
     async function fetchAllTasks() {
       const res = await fetch('http://localhost:8080/tasks/getAllTasks');
@@ -22,13 +26,16 @@ export default function TasksList() {
     fetchAllTasks();
   }, [allTasks]);
 
+  const handleClick = (taskId:string)=>{
+    router.push(`/${taskId}`);
+  }
   return (
     <div>
       <Paper sx={{ width: '30%', height: '70%', overflow: 'auto', backgroundColor: "lightgreen" }}>
         <List dense component="div" role="list">
           {allTasks.map((task, index) => {
             return (
-              <ListItem key={index} role="listitem" button>
+              <ListItem key={task.id} role="listitem" button onClick={()=>handleClick(task.id)} >
                 <ListItemIcon>
                   <Checkbox checked={task.status} tabIndex={-1} disableRipple />
                 </ListItemIcon>
